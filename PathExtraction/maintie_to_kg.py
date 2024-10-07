31dfae84ea902f45161bc181f251d61eb8b4bb3c
LisonLau <allisonlhm@gmail.com>
2024-10-07 09:50:10 +0800

    Remove irrelevant files
    - Backup files are on branch 'version'

---
+++ PathExtraction/maintie_to_kg.py
@@ -0,0 +1,125 @@
+import csv
+import json
+from neo4j import GraphDatabase
+
+# Function to create label names
+def create_label_name(tokens, start, end):
+    """ Create label name from tokens. """
+    label = " ".join(tokens[start:end])
+    label = label.lower().strip()
+    return label
+
+# Function to create nodes
+def create_nodes(tx, entities, unique_entities, tokens, entry_id):
+    """ Create nodes for entities. """
+    current_entities = []
+    for entity in entities:
+        entity_text = create_label_name(tokens, entity["start"], entity["end"])
+        entity_type = entity["type"].split('/')[0]
+        sub_types = entity["type"].split('/')[1:]
+        unique_entity_key = (entity_text, entity_type)
+        current_entities.append(unique_entity_key)
+
+        if unique_entity_key not in unique_entities:
+            entity_id = len(unique_entities)
+            unique_entities[unique_entity_key] = entity_id
+            properties = {"id": entity_id, "text": entity_text, "type": entity_type, "entry_id": [entry_id]}
+            query = f"MERGE (n:{entity_type} {{id: $id, text: $text, type: $type, entry_id: $entry_id}}) "
+            for i, subtype in enumerate(sub_types):
+                properties[f"subtype{i}"] = subtype
+                query += f"SET n.subtype{i} = $subtype{i} "
+            tx.run(query, properties)
+        else:
+            entity_id = unique_entities[unique_entity_key]
+            tx.run(
+                "MATCH (n) WHERE n.id = $id "
+                "SET n.entry_id = n.entry_id + $entry_id",
+                id=entity_id, entry_id=[entry_id]
+            )
+    return current_entities, unique_entities
+
+# Function to create relations
+def create_relations(tx, relations, unique_entities, current_entities):
+    """ Create relations between entities. """
+    for relation in relations:
+        head = relation["head"]
+        tail = relation["tail"]
+        rel_type = relation["type"].replace("/", "_")
+        head_id = unique_entities[current_entities[head]]
+        tail_id = unique_entities[current_entities[tail]]
+        tx.run(
+            f"MATCH (a {{id: $head_id}}), (b {{id: $tail_id}}) "
+            "WHERE NOT a:Entry AND NOT b:Entry "
+            f"MERGE (a)-[:{rel_type}]->(b)", 
+            head_id=head_id, tail_id=tail_id, type=rel_type
+        )
+
+# Function to create entry nodes and connect to its entities
+def create_entry(tx, entry_text, entry_id):
+    """ Create nodes for entries and connect to its entities. """
+    # Create entry node
+    tx.run(
+        "MERGE (n:Entry {id: $id, text: $text})",
+        id=entry_id, text=entry_text
+    )
+    # Connect entry node to its entities
+    tx.run(
+        "MATCH (a:Entry {id: $id}), (b) "
+        "WHERE ANY(eid IN b.entry_id WHERE eid = $entry_id) "
+        "MERGE (b)-[:comesFrom]->(a)",
+        id=entry_id, entry_id=entry_id
+    )
+
+# Function to read MaintIE manual mapping of failure mode codes
+def read_failure_mode_mapping(filepath):
+    with open(filepath, 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        next(reader)
+        failure_mode_mapping = {}
+        for row in reader:
+            if row[3] != "":
+                failure_mode_mapping[row[0]] = row[3]
+    return failure_mode_mapping # entry_text: failure_mode
+
+# Function to label entry to failure mode codes
+def entry_failure_mode(tx, failure_mode_mapping):
+    for entry_text, failure_mode in failure_mode_mapping.items():
+        tx.run(
+            "MATCH (a:Entry {text: $entry_text}) "
+            "SET a.failure_mode = $failure_mode",
+            entry_text=entry_text, failure_mode=failure_mode
+        )
+
+# Function to create graph
+def create_graph(tx, data):
+    """ Create graph from MaintIE dataset. """
+    unique_entities = {}
+    for entry_id, entry in enumerate(data):
+        entities = entry["entities"]
+        relations = entry["relations"]
+        tokens = entry["tokens"]
+        current_entities, unique_entities = create_nodes(tx, entities, unique_entities, tokens, entry_id)
+        create_relations(tx, relations, unique_entities, current_entities)
+        create_entry(tx, entry['text'], entry_id) 
+        entry_failure_mode(tx, read_failure_mode_mapping('data/MaintIE/gold_undesirable_mapped.csv'))
+
+    print(f"Created {len(unique_entities)} entities.")
+    print(f"Created {len(data)} entry entities.")
+    print(f"Total number of nodes: {len(unique_entities) + len(data)}")
+
+if __name__ == "__main__":
+    # Connect to Neo4j
+    URI = "bolt://localhost:7687"
+    USERNAME = "neo4j"
+    PASSWORD = "password"
+    driver = GraphDatabase.driver(URI, auth=(USERNAME, PASSWORD))
+
+    # Get data from MaintIE dataset
+    with open('../data/MaintIE/gold_release.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    # Process data
+    with driver.session() as session:
+        session.execute_write(create_graph, data)
+
+    driver.close()
--- README.md
+++ README.md
@@ -1,46 +1,52 @@
 # Hons24_AllisonLau
 ## Instructions
+### Datasets
+
+The datasets can be found in the `data` directory. The following datasets are used/analysed in this project:
+
+- [FMC-MWO2KG dataset](https://paperswithcode.com/dataset/fmc-mwo2kg): labelled dataset of failure mode classification (FMC) from failure observations
+- [MaintIE dataset](https://github.com/nlp-tlp/maintie): annotated dataset of maintenance work orders with entities and relations in the MaintIE schema
+- [MaintNorm dataset](https://github.com/nlp-tlp/maintnorm): maintenance work order dataset with pre- and post-lexically normalised text (combined)
+- [Corrections dictionary](): dictionary of corrections used for the humanisation step of synthetic data generation pipeline
+
 ### Dataset Analysis
-1. Run `python data_analysis.py` to analyse MaintIE or FMC-MWO2KG datasets.
 
-This python script contains code for dataset preparation and dataset analysis.
+The data analysis code can be found in the `DataAnalysis` directory. The following analyses are performed:
 
-### Load MaintIE Dataset into Neo4j Knowledge Graph
+- `mwo2kg_analysis.py`: analysis of the FMC-MWO2KG dataset - frequency of failure mode codes (classes), aligning the failure observations with the MWOs in the dataset
+- `maintie_analysis.py`: analysis of the MaintIE dataset - frequency of total entities and relations, frequency of unique entities and relations, types of triples, number of tokens in the MWOs (min, max, average), number of hierarchical relations between equipment and their components, extract undesirable events (failure modes), analysis of manual FMC mapping, frequency distribution of sentence lengths
+- `maintnorm_analysis.py`: extract relevant pairs of pre- and post-lexically normalised MWO tokens for compiling a corrections dictionary
+- `synthetic_analysis.py`: number of tokens in the MWOs (min, max, average), sentence length distribution (human vs synthetic data), filter potential LLM hallucinations for analysis (output not strictly following given prompt)
+- `typos_analysis.py`: loading corrections dictionaries, reordering corrections dictionaries
+
+### Equipment-Failure Path Extraction from MaintIE Knowledge Graph (Neo4j)
+
+The code for extracting equipment-failure paths from the MaintIE Knowledge Graph can be found in the `PathExtraction` directory. The following steps are performed:
+
+#### Load MaintIE Dataset into Neo4j Knowledge Graph
+
 1. Create a project and an instance of a graph database in Neo4j Desktop.
 2. Start the database and open the browser.
 3. Run `python maintie_to_kg.py` to load the MaintIE dataset into Neo4j.
 
-### Extracting paths (equipment and undesirable event combination) from Neo4j
+#### Extracting paths (equipment and undesirable event combination) from Neo4j
 1. Queries for extracting paths are stored in `path_queries.py`.
 2. Run `python path_matching.py` to extract paths from Neo4j.
 3. Different paths are stored in their respective json files in `path_patterns` directory.
+4. Analysis of paths can be found - total number of paths, frequency of equipment, frequency of undesirable events, frequency of inherent function of *PhysicalObjects*.
 
-### LLM Generate data given Path (equipment and undesirable event combination)
-1. Run `python llm_humanise.py` to generate MWO sentences from valid paths.
+### MWO Sentence Generation via LLM (GPT-4o mini)
 
-### Flair Text Classification
-1. Run `python flair_fmc.py` to train the flair text classification model.
 
-- `train_fmc(dir_name)`: Train the flair text classification model for FMC.
-- `test_fmc(dir_name)`: Test the flair text classification model for FMC.
-### LLM Generate data given Failure Mode Code
-1. Run `python llm_generate.py` to generate data.
 
-```python
-generate_data(gpt_model, output_dir, is_fewshot, is_specific):
-```
-- `gpt_model`: The GPT model to generate data.
-- `output_dir`: The directory to save the generated data.
-- `is_fewshot`: Whether to use few-shot learning.
-- `is_specific`: Whether to use specific prompting.
+### MWO Sentence Humanisation via Rule-based Approach (Probabilistic Rules)
 
-### Fine-tuning
-1. Run `python finetune_model.py` to fine-tune the model.
+#### Introduce Contractions
 
-Fine-tuning jobs for each model can be found under the `FinetuneModels` directory. These models can be used as the `gpt_model` parameter in the `generate_data` function.
 
-### Prepare data
-1. Run `python prepare_data.py` to prepare data for training.
+#### Introduce Abbreviations
 
-- `prepare_data_for_llm()`: Prepare data for training the GPT-3.5 model.
-- `prepare_data_for_fmc(dir_name)`: Prepare data for fine-tuning the flair text classification model for FMC.
+#### Introduce Typographical Errors
+
+
+###
